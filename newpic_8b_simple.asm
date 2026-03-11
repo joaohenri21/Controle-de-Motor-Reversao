aefb3ed1512862ce55403b8a8965498737afe5bc
João Henrique <joao.hr@estudante.iftm.edu.br>
2026-03-11 09:40:24 -0300

    Mudanças nos botões
    Mudança em que, com o botão desligado pressionado, não é possivel ligar o motor anti-horario e horario.

--- newpic_8b_simple.asm
+++ newpic_8b_simple.asm
@@ -5,8 +5,8 @@
 ; __config 0xFF70
     __CONFIG _FOSC_INTOSCIO & _WDTE_OFF & _PWRTE_ON & _MCLRE_ON & _BOREN_ON & _LVP_OFF & _CPD_OFF & _CP_OFF
 
-#define	BANK0	BCF STATUS,RP0	    ;seleciona banco 0 da mem髍ia RAM
-#define	BANK1	BSF STATUS,RP0	    ;seleciona banco 1 da mem髍ia RAM
+#define	BANK0	BCF STATUS,RP0	    ;seleciona banco 0 da mem贸ria RAM
+#define	BANK1	BSF STATUS,RP0	    ;seleciona banco 1 da mem贸ria RAM
 
  
 ;entradas
@@ -14,7 +14,7 @@
 #define LIG_ANTIHOR	PORTA,2
 #define DESL		PORTA,3
 #define LIGA_EMERG	PORTA,4
-;sa韉as
+;sa铆das
 #define	LIGADO		PORTA,0
 #define	HORARIO		PORTB,0
 #define ANTIHOR		PORTB,1
@@ -22,13 +22,13 @@
 #define EMERG_LIGADO	PORTB,3
   
 RES_VECT  CODE    0x0000            ; processor reset vector
-    BANK1			    ;seleciona banco 1 da mem髍ia RAM
+    BANK1			    ;seleciona banco 1 da mem贸ria RAM
     BCF	    TRISA,0
     BCF	    TRISB,0		    
     BCF	    TRISB,1
     BCF	    TRISB,2
     BCF	    TRISB,3
-    BANK0			    ;seleciona banco 0 da mem髍ia RAM
+    BANK0			    ;seleciona banco 0 da mem贸ria RAM
     BSF	    DESLIGADO		    
     BCF	    LIGADO
     BCF	    HORARIO
@@ -53,12 +53,18 @@ PRINCIPAL4
     GOTO    PRINCIPAL1
 EMERGENCIA
     BSF	    EMERG_LIGADO
-    BCF	    DESLIGADO
+    BSF	    DESLIGADO
     BCF	    LIGADO
     BCF	    HORARIO
     BCF	    ANTIHOR
     GOTO    PRINCIPAL2
 LIGA_HORARIO
+    BTFSC   ANTIHOR
+    GOTO    PRINCIPAL3
+    BTFSS   LIG_ANTIHOR
+    GOTO    PRINCIPAL3
+    BTFSS   DESL
+    GOTO    PRINCIPAL3
     BTFSC   EMERG_LIGADO
     GOTO    PRINCIPAL3
     BSF	    HORARIO
@@ -67,6 +73,12 @@ LIGA_HORARIO
     BCF	    DESLIGADO
     GOTO    PRINCIPAL3
 LIGA_ANTIHORARIO
+    BTFSC   HORARIO
+    GOTO    PRINCIPAL4
+    BTFSS   LIG_HORARIO
+    GOTO    PRINCIPAL4
+    BTFSS   DESL
+    GOTO    PRINCIPAL4
     BTFSC   EMERG_LIGADO
     GOTO    PRINCIPAL4
     BSF	    ANTIHOR
@@ -74,14 +86,12 @@ LIGA_ANTIHORARIO
     BCF	    HORARIO
     BCF	    DESLIGADO
     GOTO    PRINCIPAL4
-DESLIGA
+DESLIGA    
     BSF	    DESLIGADO
     BCF	    HORARIO
     BCF	    ANTIHOR
     BCF	    LIGADO
     BCF	    EMERG_LIGADO
     GOTO    PRINCIPAL1
-
-GOTO PRINCIPAL1
     
     END
